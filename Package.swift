582238b7a9be369abae9ecf38a99dc222c8690e1
Simone Deriu <simone.deriu94@gmail.com>
2020-05-15 17:22:34 +0200

    Updated package name

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@ import PackageDescription
 let package = Package(
     name: "DOSpaces",
     products: [
-        .library(name: "DOSpaces", targets: ["App"]),
+        .library(name: "DOSpaces", targets: ["DOSpaces"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/rausnitz/S3.git", .branch("master"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["S3Signer", "Vapor"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+        .target(name: "DOSpaces", dependencies: ["S3Signer", "Vapor"]),
+        .testTarget(name: "AppTests", dependencies: ["DOSpaces"])
     ]
 )
 
