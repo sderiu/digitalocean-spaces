d872836326c0a1b6888c0d3a62f8b9b2d08ccea2
Simone Deriu <simone.deriu94@gmail.com>
2020-05-21 17:50:40 +0200

    Fixed dependency

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/rausnitz/S3.git", .branch("master")),
-        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser", from: "5.2.0")
+        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser.git", from: "5.2.0")
     ],
     targets: [
         .target(name: "DOSpaces", dependencies: ["S3Signer", "Vapor", "SwiftyXMLParser"]),
